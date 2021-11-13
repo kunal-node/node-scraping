61d20421c02eadaaca8779076cd14fad497545e4
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2021-11-13 22:42:31 +0530

    puppeteer scraping job titles using cheerio

--- 3.scraping-craigslist/src/index.js
+++ 3.scraping-craigslist/src/index.js
@@ -1,10 +1,22 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
 const main = async () => {
   //headless false means show the browser.
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.google.com");
+  await page.goto(
+    "https://pune.craigslist.org/d/software-qa-dba-etc/search/sof"
+  );
+  const html = await page.content();
+  const $ = cheerio.load(html);
+  // const titles = $("a.result-title");
+  const titles = $(
+    "ul#search-results li.result-row div.result-info h3.result-heading a.result-title"
+  );
+  titles.each((index, element) => {
+    console.log(index, " : ", $(element).text());
+  });
 };
 
 main();
