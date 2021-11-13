6a9abab2750a7320a575b867095b4e4146be561e
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2021-11-13 20:48:07 +0530

    scraping table with dynamic table headers

--- 2.scraping-html-table/src/index.js
+++ 2.scraping-html-table/src/index.js
@@ -13,7 +13,7 @@ const main = async () => {
   }
   fs.writeFileSync("./test.html", html);
   let $ = cheerio.load(html);
-  const scrapedRows = [];
+  let scrapedRows = [];
   $("body > table > tbody > tr").each((index, element) => {
     // if th return.
     if (index === 0) {
@@ -25,6 +25,34 @@ const main = async () => {
     const country = $(tds[2]).text();
     const scrapedRow = { company, country, contact };
     scrapedRows.push(scrapedRow);
+  });
+  console.log("scrapedData : ", scrapedRows);
+
+  // 2. scraping table data(tr td) with dynamic header(th).
+  try {
+    html = await request.get("https://www.codingwithstefan.com/table-example/");
+  } catch (err) {
+    console.log(err);
+  }
+  fs.writeFileSync("./test.html", html);
+  $ = cheerio.load(html);
+  const tableHeaders = [];
+  scrapedRows = [];
+  $("body > table > tbody > tr").each((index, element) => {
+    if (index === 0) {
+      const ths = $(element).find("th");
+      ths.each((index, element) => {
+        tableHeaders.push($(element).text().toLowerCase());
+      });
+    } else {
+      const tds = $(element).find("td");
+      const tableRow = {};
+      tds.each((index, element) => {
+        const td = $(element).text();
+        tableRow[tableHeaders[index].toLowerCase()] = td;
+      });
+      scrapedRows.push(tableRow);
+    }
   });
   console.log("scrapedData : ", scrapedRows);
 };
