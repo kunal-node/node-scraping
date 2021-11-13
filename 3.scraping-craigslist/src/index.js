b339840255098a2c28a390afa32e7f3f05337f9a
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2021-11-14 00:58:18 +0530

    pupeteer scraping neighborhood data

--- 3.scraping-craigslist/src/index.js
+++ 3.scraping-craigslist/src/index.js
@@ -11,15 +11,17 @@ const main = async () => {
   const html = await page.content();
   const $ = cheerio.load(html);
   // const titles = $("a.result-title");
-  const resultInfo = $("ul#search-results li.result-row div.result-info");
-  const jobs = resultInfo
+  const results = $("ul#search-results li.result-row div.result-info");
+  const jobs = results
     .map((index, element) => {
       const titleElement = $(element).find("h3.result-heading a.result-title");
       const timeElement = $(element).find("time.result-date");
+      const hoodElement = $(element).find("span.result-meta span.result-hood");
       const title = titleElement.text();
       const url = titleElement.attr("href");
       const datePosted = new Date(timeElement.attr("datetime"));
-      return { title, url, datePosted };
+      const hood = hoodElement.text().trim().replace("(", "").replace(")", "");
+      return { title, url, datePosted, hood };
     })
     .get(); // map return the cheerio object's, hence we have to use get method on top of map to get normal objects.
   console.log("jobs : ", jobs);
