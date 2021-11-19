286cfd67ad69e767b72cf56b918cb75f103d9f1b
Kunal Pardeshi <kunalpardeshi77@gmail.com>
2021-11-19 18:27:39 +0530

    2.added meaningful comments

--- 6.robots-parser-with-example-site/src/index.js
+++ 6.robots-parser-with-example-site/src/index.js
@@ -1,5 +1,6 @@
 var robotsParser = require("robots-parser");
 
+//Get the robots.txt file with request package, and pass it as 2nd argument.
 var robots = robotsParser(
   "http://www.example.com/robots.txt",
   [
@@ -14,6 +15,7 @@ var robots = robotsParser(
 );
 
 console.log(
+  //2nd argument(Sams-Bot/1.0) is a User-Agent string.
   robots.isAllowed("http://www.example.com/test.html", "Sams-Bot/1.0")
 ); // false
 console.log(
